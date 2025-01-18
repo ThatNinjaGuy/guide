00dba9539d01948486eba2b78d3e612cb847ced7
ThatNinjaGuy <vishalgautam.tech@gmail.com>
2025-01-19 03:30:09 +0530

    Updated a theme provider to manage theme centrally.

--- guide-ui/src/App.js
+++ guide-ui/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import ThemeProvider from "./components/ThemeProvider";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -9,17 +10,19 @@ import SearchResults from "./components/SearchResults";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/search-results" element={<SearchResults />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/search-results" element={<SearchResults />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
