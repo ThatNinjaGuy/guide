9ea9d9d952e3aed34279852315918508bdb4ecaa
ThatNinjaGuy <vishalgautam.tech@gmail.com>
2025-01-20 18:11:09 +0530

    Updated assetPrefix path

--- guide-ui/next.config.ts
+++ guide-ui/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/mentor-guide",
-  assetPrefix: "/mentor-guide",
+  // basePath: "/mentor-guide",
+  // assetPrefix: "/mentor-guide",
 };
 
 export default nextConfig;
